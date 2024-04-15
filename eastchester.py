8450d3f7dc1b812727e8c0991f5e0615e85d8e7d
Truman2557 <144353279+Truman2557@users.noreply.github.com>
2024-04-15 14:14:21 +0000

    silyy

--- eastchester.py
+++ eastchester.py
@@ -3,5 +3,19 @@ from bs4 import BeautifulSoup
 import codecs
 import re
 
-eastchester_scrape = requests.get("https://www.eufsdk12.ord/domains/76")
+eastchester_scrape = requests.get("https://www.eufsdk12.org/domains/76")
 soup = BeautifulSoup(eastchester_scrape.text, "html.parser")
+
+email_elements = soup.findAll("li", class_="staffemail")
+decoded_emails = []
+
+for element in email_elements:
+    script = element.find("script").string if element.find("script") else None
+    if script: 
+        matches = re.findall(r"swrot13\('([^']+)'\)", script)
+        if matches:
+            encoded_email = matches[0]
+            decoded_email codecs.decode(encoded_email, 'rot_13')
+            decoded_emails.append(decoded_email)
+for email in decoded_emails: 
+    print(email)
